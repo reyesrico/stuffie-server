b5c47ec84bbc9f86c1429f65ad0ff04e866ec95f
Carlos Reyes <reyesrico@hotmail.com>
2022-04-18 12:20:08 -0700

    Add express and index.html

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const server = require('http').createServer();
-const WebSocket = require('ws');
+// Using WebSockets on Heroku
+// https://devcenter.heroku.com/articles/node-websockets
 
-server.listen(8080);
-console.log('Listening on port 8080');
+const express = require('express');
+const { Server } = require('ws');
 
-const socket = new WebSocket.Server({ server:server });
+// const server = require('http').createServer();
+// const WebSocket = require('ws');
+
+
+const PORT = 8080;
+const INDEX = '/index.html';
+// server.listen(8080);
+// console.log('Listening on port 8080');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const socket = new Server({ server });
 
 socket.on('connection', ws => {
-  ws.send('Welcome!!!');
+  ws.send('Client connected');
 
   ws.on('message', (data, isBinary) => {
 
@@ -16,16 +29,18 @@ socket.on('connection', ws => {
     console.log(`Messsage Received: ${message}`);
     console.log(typeof message);
 
+    console.log(`clients connected ${socket.clients.size}`);
     // Broadcasting message to all connected clients
     socket.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      // if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
-      }
+      // }
     });
   });
 
   ws.on('close', () => {
+    console.log('Client disconnected');
     // console.log(`Client ${userId} has disconnected`);
     // delete clients.userId;
   });
-});
+}, 1000);
